b25d9b6976eb9a79e696d8bdef7d59929992e1b3
JudyYeon <dev.judykim@gmail.com>
2023-10-08 08:53:37 +0900

    - swagger server 설정

--- src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/config/SwaggerConfiguration.java
+++ src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/config/SwaggerConfiguration.java
@@ -2,19 +2,40 @@ package kr.co.wanted.judy.wantedpreonboardingbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Collections;
+
 @Configuration
+@EnableOpenApi
 public class SwaggerConfiguration {
     @Bean
     public Docket api(){
+        Server serverLocal = new Server("local", "http://localhost:8080", "for local usages", Collections.emptyList(), Collections.emptyList());
+
         return new Docket(DocumentationType.OAS_30)
+                .servers(serverLocal)
+                .groupName("api")
+                .apiInfo(this.apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("kr.co.wanted.judy.wantedpreonboardingbackend.controller"))
+                .paths(PathSelectors.ant("/api/v1/**"))
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Recruit Sever API")
+                .description("프리온보딩 과제 Recruit API에 대한 연동문서 Documentation 입니다.")
+                .version("1.0.0")
                 .build();
     }
 }
---
+++ src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/service/Workaround.java
@@ -0,0 +1,31 @@
+package kr.co.wanted.judy.wantedpreonboardingbackend.service;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.stereotype.Component;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Component
+public class Workaround implements WebMvcOpenApiTransformationFilter {
+    @Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+        OpenAPI openApi = context.getSpecification();
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("http://localhost:8080");
+
+        openApi.setServers(Collections.singletonList(localServer));
+        return openApi;
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return documentationType.equals(DocumentationType.OAS_30);
+    }
+}
