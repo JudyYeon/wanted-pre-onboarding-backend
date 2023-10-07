57773c4c98f89dc44f009e6645f73863eefe1160
JudyYeon <dev.judykim@gmail.com>
2023-10-08 08:04:21 +0900

    - controller, service layout

--- src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/controller/RecruitController.java
+++ src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/controller/RecruitController.java
@@ -1,19 +1,35 @@
 package kr.co.wanted.judy.wantedpreonboardingbackend.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @Api(tags = {"Recruit Controller"})
+@RequestMapping(value = "/api/v1/wanted-recruit/")
+@RequiredArgsConstructor
 public class RecruitController {
 
+    @ApiOperation(value = "Adding notice")
+    @RequestMapping(value = "/notice", method = RequestMethod.POST)
     public void noticeAdd(){
 
     }
+
+    @ApiOperation(value = "Adding notice")
+    @RequestMapping(value = "/notice", method = RequestMethod.GET)
     public void noticeList(){
 
     }
 
+    @ApiOperation(value = "Adding notice")
+    @RequestMapping(value = "/notice-detail", method = RequestMethod.GET)
     public void noticeDetail(){
 
     }
--- src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/service/RecruitService.java
+++ src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/service/RecruitService.java
@@ -1,13 +1,13 @@
 package kr.co.wanted.judy.wantedpreonboardingbackend.service;
 
-import kr.co.wanted.judy.wantedpreonboardingbackend.domain.RecruitNotice;
+import kr.co.wanted.judy.wantedpreonboardingbackend.domain.Notice;
 
 import java.util.List;
 
 public interface RecruitService {
 
     // 요구사항 1. 등록 유형의 서비스
-    void addNotice(RecruitNotice recruitNoticeDTO);
+    void addNotice(Notice recruitNoticeDTO);
 
     // 요구사항 2. 수정 유형의 서비스
     void modifyNotice();
@@ -16,13 +16,13 @@ public interface RecruitService {
     void removeNotice();
     
     //요구사항 4-1. 목록조회 유형의 서비스
-    List<RecruitNotice> findNotice();
+    List<Notice> findNotice();
     
     //요구사항 4-2. 검색조회 유형의 서비스
-    List<RecruitNotice> searchNotice();
+    List<Notice> searchNotice();
 
     // 요구사항 5. 상세조회 유형의 서비스
-    RecruitNotice findNoticeDetail(int id);
+    Notice findNoticeDetail(int id);
 
     // 요구사항 6. 지원자 등록 유형의 서비스
 
