998abe8986422db837019d4baf4a676bab8bb2db
JudyYeon <dev.judykim@gmail.com>
2023-10-08 13:05:43 +0900

    - swagger config 수정

--- src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/config/SwaggerConfiguration.java
+++ src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/config/SwaggerConfiguration.java
@@ -2,40 +2,62 @@ package kr.co.wanted.judy.wantedpreonboardingbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
-@EnableOpenApi
+@EnableWebMvc
 public class SwaggerConfiguration {
     @Bean
     public Docket api(){
         Server serverLocal = new Server("local", "http://localhost:8080", "for local usages", Collections.emptyList(), Collections.emptyList());
 
         return new Docket(DocumentationType.OAS_30)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
                 .servers(serverLocal)
-                .groupName("api")
                 .apiInfo(this.apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("kr.co.wanted.judy.wantedpreonboardingbackend.controller"))
-                .paths(PathSelectors.ant("/api/v1/**"))
+                .paths(PathSelectors.ant("/api/v1/wanted-recruit/**"))
                 .build()
                 .useDefaultResponseMessages(false);
+    }
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
     }
 
     private ApiInfo apiInfo(){
         return new ApiInfoBuilder()
                 .title("Recruit Sever API")
                 .description("프리온보딩 과제 Recruit API에 대한 연동문서 Documentation 입니다.")
+                .license("Copyright 2023. printf-hellojudyworld. All Rights Reserved.")
                 .version("1.0.0")
                 .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
     }
 }
