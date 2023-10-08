b59f5e011641c1df6643476c9ce3a08054bc463a
JudyYeon <dev.judykim@gmail.com>
2023-10-08 14:32:58 +0900

    - common response 수정

--- src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/model/ApiResponseList.java
+++ src/main/java/kr/co/wanted/judy/wantedpreonboardingbackend/model/ApiResponseList.java
@@ -4,37 +4,36 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import kr.co.wanted.judy.wantedpreonboardingbackend.model.data.EnumResponseResult;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
-public class ApiResponseData<T> {
+public class ApiResponseList<T> {
 
-    public ApiResponseData(){
-        this(EnumResponseResult.ERROR, "", "", null);
-    }
-
-    public ApiResponseData(T data){
-        this(EnumResponseResult.SUCCESS, "", "", data);
-    }
-
-    public ApiResponseData(EnumResponseResult result, String code, String message, T data) {
+    public ApiResponseList(EnumResponseResult result, String code, String message, List<T> data) {
         try{
             this.result = result;
             this.code = code;
             this.message = message;
-            this.data = data;
+
+            if(data == null)
+                this.data = new ArrayList<>();
+            else
+                this.data = data;
+
         }catch (Exception e){
             e.printStackTrace();
         }
     }
-    
     @Schema(description = "응답 결과", required = true)
     EnumResponseResult result = EnumResponseResult.ERROR;
-    
+
     @Schema(description = "응답 코드", required = true)
     String code;
-    
+
     @Schema(description = "응답 메새지", required = true)
     String message;
 
     @Schema(description = "데이터", required = true)
-    T data;
+    List<T> data;
 }
